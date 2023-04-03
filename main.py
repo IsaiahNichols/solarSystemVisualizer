718edd80981d8728a5a1c9bcb9466a0236099b7d
Isaiah Nichols <isaiah.nichols180@gmail.com>
2023-04-02 23:08:31 -0700

    padded skeleton

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ dimensions = (800, 800)
 screen = pygame.display.set_mode(dimensions)
 pygame.display.set_caption("SOLAR SYSTEM")
 
+# Player Position
+pos = [0, 0]
+zoom = 1
+
 # Game Objects
 class Planet:
     def __init__(self, dist, size, speed) -> None:
@@ -18,9 +22,24 @@ class Planet:
         pass
 
 
+# --- Planets
+mercury = Planet()
+venus = Planet()
+earth = Planet()
+mars = Planet()
+jupiter = Planet()
+saturn = Planet()
+uranus = Planet()
+neptune = Planet()
+
+planets = [mercury, venus, earth, mars, jupiter, saturn, uranus, neptune]
+
 # Game Loop
 running = 1
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = 0
+    
+    for planet in planets:
+        planet.main()
