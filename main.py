138fdde42bb04247a6bca8afab85fd4f7d0f2ab0
Isaiah Nichols <isaiah.nichols180@gmail.com>
2023-04-02 22:59:22 -0700

    created skeleton

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-# Real Time Solar System Visualizer
+#Solar System Visualizer
+import pygame
+
+pygame.init()
+
+dimensions = (800, 800)
+screen = pygame.display.set_mode(dimensions)
+pygame.display.set_caption("SOLAR SYSTEM")
+
+# Game Objects
+class Planet:
+    def __init__(self, dist, size, speed) -> None:
+        self.distance = dist
+        self.size = size
+        self.speed = speed
+    
+    def main(self):
+        pass
+
+
+# Game Loop
+running = 1
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = 0
