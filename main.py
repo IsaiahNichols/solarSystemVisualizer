868fe08d8b8d741e7fd14aed5fbf8400803b78eb
Isaiah Nichols <isaiah.nichols180@gmail.com>
2023-04-04 16:28:37 -0700

    padded skeleton

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #Solar System Visualizer
 import utils
+import math
 import pygame
 
 pygame.init()
@@ -14,7 +15,30 @@ pygame.display.set_caption("SOLAR SYSTEM")
 # Player Position
 user_pos = [-dimensions[0]/2, -dimensions[1]/2]
 zoom = 1
+ZOOM_SPEED = 0.01
+USER_SPEED = 10
 
+def user_pos():
+    keys = pygame.key.get_pressed()
+    dir = [0, 0]
+
+    dir[0] = keys[pygame.K_d] - keys[pygame.K_a]
+    dir[1] = keys[pygame.K_d] - keys[pygame.K_a]
+
+    user_hypo = math.sqrt(pow(dir[0], 2) + pow(dir[1], 2))
+
+    for i in range(len(dir)):
+        dir[i] /= user_hypo
+    
+    mouse = pygame.mouse.get_pressed()
+
+    if mouse[0]:
+        zoom -= ZOOM_SPEED
+    if mouse[2]:
+        zoom += ZOOM_SPEED
+
+def user_main():
+    user_pos()
 
 # Game Objects
 class Planet:
@@ -50,8 +74,11 @@ while running:
         if event.type == pygame.QUIT:
             running = 0
     
+    # Giving function to game objects
     for planet in planets:
         planet.main()
+    
+    user_main()
 
     clock.tick(FPS)
     pygame.display.flip()
