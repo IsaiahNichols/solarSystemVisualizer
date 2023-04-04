fbb691d67439f84c7ff9970c7842d12faecbc5e2
Isaiah Nichols <isaiah.nichols180@gmail.com>
2023-04-03 21:19:01 -0700

    padded skeleton

--- main.py
+++ main.py
@@ -1,45 +1,57 @@
 #Solar System Visualizer
+import utils
 import pygame
 
 pygame.init()
+
+clock = pygame.time.Clock()
+FPS = 60
 
 dimensions = (800, 800)
 screen = pygame.display.set_mode(dimensions)
 pygame.display.set_caption("SOLAR SYSTEM")
 
 # Player Position
-pos = [0, 0]
+user_pos = [-dimensions[0]/2, -dimensions[1]/2]
 zoom = 1
+
 
 # Game Objects
 class Planet:
-    def __init__(self, dist, size, speed) -> None:
-        self.distance = dist
+    def __init__(self, pos: list, size: int) -> None:
+        self.pos = pos
+        self.rel_pos = [0, 0]
         self.size = size
-        self.speed = speed
+        self.rel_size = size
     
+    def draw(self):
+        pygame.draw.circle(screen, utils.WHITE, self.rel_pos, self.size)
+
     def main(self):
-        pass
+        for i in range(2):
+            self.rel_pos[i] = (self.pos[i] - user_pos[i]) * zoom
+
+        self.rel_size = self.size * zoom
+
+        self.draw()
 
 
 # --- Planets
-mercury = Planet()
-venus = Planet()
-earth = Planet()
-mars = Planet()
-jupiter = Planet()
-saturn = Planet()
-uranus = Planet()
-neptune = Planet()
+mercury = Planet([0, 0], 32)
 
-planets = [mercury, venus, earth, mars, jupiter, saturn, uranus, neptune]
+planets = [mercury]
 
 # Game Loop
 running = 1
 while running:
+    screen.fill((0, 0, 0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = 0
     
     for planet in planets:
         planet.main()
+
+    clock.tick(FPS)
+    pygame.display.flip()
