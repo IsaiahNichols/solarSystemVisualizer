247e57484083b82b5fa73ec2c7ea33a2d3a50090
Isaiah Nichols <isaiah.nichols180@gmail.com>
2023-04-04 17:17:39 -0700

    added basics functions

--- main.py
+++ main.py
@@ -15,30 +15,39 @@ pygame.display.set_caption("SOLAR SYSTEM")
 # Player Position
 user_pos = [-dimensions[0]/2, -dimensions[1]/2]
 zoom = 1
-ZOOM_SPEED = 0.01
+ZOOM_SPEED = 0.1
 USER_SPEED = 10
 
-def user_pos():
+def update_user_pos():
+    global zoom
+    global ZOOM_SPEED
+    global user_pos
     keys = pygame.key.get_pressed()
     dir = [0, 0]
 
     dir[0] = keys[pygame.K_d] - keys[pygame.K_a]
-    dir[1] = keys[pygame.K_d] - keys[pygame.K_a]
+    dir[1] = keys[pygame.K_s] - keys[pygame.K_w]
 
     user_hypo = math.sqrt(pow(dir[0], 2) + pow(dir[1], 2))
 
-    for i in range(len(dir)):
-        dir[i] /= user_hypo
-    
-    mouse = pygame.mouse.get_pressed()
+    for i in range(2):
+        if user_hypo: dir[i] /= user_hypo
 
+    user_pos[0] += dir[0] * USER_SPEED
+    user_pos[1] += dir[1] * USER_SPEED
+
+    mouse = pygame.mouse.get_pressed()
+    
     if mouse[0]:
-        zoom -= ZOOM_SPEED
-    if mouse[2]:
         zoom += ZOOM_SPEED
+    if mouse[2]:
+        zoom -= ZOOM_SPEED
+    
+    if keys[pygame.K_0]:
+        user_pos = [-dimensions[0]/2, -dimensions[1]/2]
 
 def user_main():
-    user_pos()
+    update_user_pos()
 
 # Game Objects
 class Planet:
@@ -49,9 +58,10 @@ class Planet:
         self.rel_size = size
     
     def draw(self):
-        pygame.draw.circle(screen, utils.WHITE, self.rel_pos, self.size)
+        pygame.draw.circle(screen, utils.WHITE, self.rel_pos, self.rel_size)
 
     def main(self):
+        global zoom
         for i in range(2):
             self.rel_pos[i] = (self.pos[i] - user_pos[i]) * zoom
 
